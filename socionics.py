81e6aa4717bd35d8b295e675ee42cc0083383200
Oleh Protsenko <designofhumanity@gmail.com>
2023-07-29 10:49:57 +0300

    more professional code

--- socionics.py
+++ socionics.py
@@ -1,11 +1,12 @@
-# Intertype relations between types (0 - Identity, 1 - Duality, etc.)
-relation_names = [
+"""Module for calculating the relationship between two socionic types."""
+
+TYPE_RELATIONSHIPS = [
     'Identity', 'Duality', 'Activation', 'Mirror', 'Benefactor', 'Supervisee',
     'Business', 'Mediator', 'Quasi-identity', 'Extinguishment', 'Conflict',
-    'Beneficiary', 'Supervisor', 'Kindred', 'Semi-duality', 'Activation']
+    'Beneficiary', 'Supervisor', 'Kindred', 'Semi-duality', 'Activation'
+]
 
-# Descriptions of Intertype relationships
-type_description = [
+TYPE_DESCRIPTIONS = [
     'These partners are very similar in their abilities and the way they look at the world as many of the same traits are shared. This relationship is the easiest to form and will succeed when there is a need to transfer accumulated knowledge and experience back and forth. Both will have complete understanding but due to similarities will be largely unable to help the other with identical problems.',
     'Interaction is generally rewarding and satisfying for both, providing each other with inspiration and support. This is often considered the most beneficial relation as all interactions are complementary, providing an “other half” effect.',
     'Partners raise the overall energy level, serve to mobilize one another. However, these partners tend to overactivate each other over time and can do with breaks in order to cool down.',
@@ -21,40 +22,37 @@ type_description = [
     'Both partners greatly admire each other. However, there is an asymmetry: the benefactor is able to help the beneficiary but not the other way around. As a result, these relations are often temporary, with a sense of unfairness being felt over time by the benefactor and a sense of uselessness being felt by the beneficiary. ',
     'This is an asymmetric relationship, in which one partner is constantly seen as lacking in some required area by the other. As a result, the supervisee feels somewhat wary or careful about their words and actions around the supervisor, feeling that they could be corrected in an area they feel insecure about.',
     'Partners like to discuss similar topics and often have many common interests. But when they come to practical implementation of their mutual goals and projects, each finds that the other acts in contrary ways.',
-    'Relationships of semi-duality can become very close for moderate periods of time. Often these relations lead to friendship and fruitful cooperation at work. But very close relationships can lead to disappointments.']
+    'Relationships of semi-duality can become very close for moderate periods of time. Often these relations lead to friendship and fruitful cooperation at work. But very close relationships can lead to disappointments.'
+]
 
-# Dictionary mapping type abbreviations to their index in the type_name and type_abbr lists
-type_abbr = {
+TYPE_ABBREVIATIONS = {
     'ILE': 0, 'SEI': 1, 'ESE': 2, 'LII': 3, 'EIE': 4, 'LSI': 5, 'SLE': 6, 'IEI': 7,
     'SEE': 8, 'ILI': 9, 'LIE': 10, 'ESI': 11, 'LSE': 12, 'EII': 13, 'IEE': 14, 'SLI': 15
 }
 
-# Dictionary mapping type index to their name
-type_name = {
+TYPE_NAMES = {
     0: 'Innovator', 1: 'Mediator', 2: 'Enthusiast', 3: 'Analyst', 4: 'Mentor', 5: 'Inspector',
     6: 'Marshal', 7: 'Romantic', 8: 'Leader', 9: 'Critic', 10: 'Entrepreneur', 11: 'Moralist',
     12: 'Pragmatist', 13: 'Humanist', 14: 'Networker', 15: 'Ergonomist'
 }
 
-# Function to calculate and display the relationship between two types
-def calculate_relationship(type1, type2):
-    code1, code2 = type_abbr[type1.upper()], type_abbr[type2.upper()]
+def calculate_type_relationship(type1, type2):
+    """Calculate and display the relationship between two socionic types."""
+    code1, code2 = TYPE_ABBREVIATIONS[type1.upper()], TYPE_ABBREVIATIONS[type2.upper()]
     relation_index = abs(code1 - code2) % 16
-    relation_name = relation_names[relation_index]
-    print(f"\nIntertype relationship between {type1.upper()} ({type_name[code1]}) and "
-          f"{type2.upper()} ({type_name[code2]}) - {relation_name} Relationship")
-    print("Description:", type_description[relation_index])
+    relation_name = TYPE_RELATIONSHIPS[relation_index]
+    print(f"\nIntertype relationship between {type1.upper()} ({TYPE_NAMES[code1]}) and "
+          f"{type2.upper()} ({TYPE_NAMES[code2]}) - {relation_name} Relationship")
+    print("Description:", TYPE_DESCRIPTIONS[relation_index])
 
-# Main function
-def main():
+if __name__ == "__main__":
     print("Welcome to the Socionics Intertype Relationship Calculator!")
-
     while True:
         type1 = input("\nEnter the first type (e.g., 'SEI' or 'Mediator'): ").upper()
-        if type1 in type_abbr:
+        if type1 in TYPE_ABBREVIATIONS:
             type2 = input("Enter the second type (e.g., 'SEI' or 'Mediator'): ").upper()
-            if type2 in type_abbr:
-                calculate_relationship(type1, type2)
+            if type2 in TYPE_ABBREVIATIONS:
+                calculate_type_relationship(type1, type2)
             else:
                 print("Invalid type abbreviation. Please enter a valid type abbreviation.")
         else:
@@ -65,6 +63,3 @@ def main():
         if exit_choice == 'no':
             print("Exiting...")
             break
-
-if __name__ == "__main__":
-    main()
