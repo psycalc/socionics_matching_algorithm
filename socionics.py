41870faa8133371315288dee35f79dae7cc2d78e
Oleh Protsenko <designofhumanity@gmail.com>
2023-07-29 11:12:36 +0300

    all my work

--- socionics.py
+++ socionics.py
@@ -1,58 +1,43 @@
-"""Module for calculating the relationship between two socionic types."""
+from socionics_relationships import RELATIONSHIP_NAMES, RELATIONSHIP_DESCRIPTIONS
+from socionics_types import TYPE_ABBREVIATIONS, TYPE_NAMES
 
-TYPE_RELATIONSHIPS = [
-    'Identity', 'Duality', 'Activation', 'Mirror', 'Benefactor', 'Supervisee',
-    'Business', 'Mediator', 'Quasi-identity', 'Extinguishment', 'Conflict',
-    'Beneficiary', 'Supervisor', 'Kindred', 'Semi-duality', 'Activation'
-]
+def get_relation_index(code1, code2):
+    """Calculate the index of the relationship between two socionic types."""
+    return abs(code1 - code2) % len(RELATIONSHIP_NAMES['en'])
 
-TYPE_DESCRIPTIONS = [
-    'These partners are very similar in their abilities and the way they look at the world as many of the same traits are shared. This relationship is the easiest to form and will succeed when there is a need to transfer accumulated knowledge and experience back and forth. Both will have complete understanding but due to similarities will be largely unable to help the other with identical problems.',
-    'Interaction is generally rewarding and satisfying for both, providing each other with inspiration and support. This is often considered the most beneficial relation as all interactions are complementary, providing an “other half” effect.',
-    'Partners raise the overall energy level, serve to mobilize one another. However, these partners tend to overactivate each other over time and can do with breaks in order to cool down.',
-    'Partners will feel as though they are looking in the mirror, seeing a reflection of their motives; will have similar actions and motivations, and can reach mutual understanding quite easily. However, levels of energy and discipline will be reversed, resulting in different lifestyles. They are likely to have slightly different takes on the same issue and may mutually correct each other. While mostly affable, sometimes, this can result in minor clashes.',
-    'Both partners greatly admire each other. However, there is an asymmetry: the benefactor is able to help the beneficiary but not the other way around. As a result, these relations are often temporary, with a sense of unfairness being felt over time by the benefactor and a sense of uselessness being felt by the beneficiary.',
-    'This is an asymmetric relationship, in which one partner is constantly seen as lacking in some required area by the other. As a result, the supervisee feels somewhat wary or careful about their words and actions around the supervisor, feeling that they could be corrected in an area they feel insecure about.',
-    'Both partners understand each other very well and are usually happy to interact. But, in general, this is a pair of people with very different goals.',
-    'These relationships are usually pleasant, but only for mutual leisure time. It is difficult to accomplish something serious together because their individual goals continually undo each other.',
-    'These are relationships built on mutual respect. Each partner sees qualities in the other that are desired. But they must maintain some psychological distance, because one-on-one, they can start to annoy one another. Although having very similar energy levels and lifestyles, they have very different abilities and life values, leading to misunderstanding.',
-    'These partners have many common interests, but are often confused by each other`s seemingly counter-intuitive insights. Their thinking styles are very different despite being good at similar things. Each may often feel that they other is deliberately trying to undermine or extinguish their approach, but this is not intentionally the case.',
-    'Partners` interests are aligned, but at a closer distance misunderstandings arise. Collaborative work with ample room to move, regarding process and approach, is necessary for success in this pair. Working in close contact will lead to confusion due to the very different values and attitudes these partners have. They will not take each other`s advice as they each perceive themselves as doing the same as the other, only better.',
-    'Misunderstandings arise very easily in this type of relationship. The two partners are exact opposites in abilities, temperament and life values, resulting in there being very little in common. Despite being initially attracted to each other due to them providing in certain amounts what the other partner needs in their life, they soon realise that the value dissonance makes relations mutually painful, with each putting pressure upon the other`s points of insecurity rather than protecting and helping those points.',
-    'Both partners greatly admire each other. However, there is an asymmetry: the benefactor is able to help the beneficiary but not the other way around. As a result, these relations are often temporary, with a sense of unfairness being felt over time by the benefactor and a sense of uselessness being felt by the beneficiary. ',
-    'This is an asymmetric relationship, in which one partner is constantly seen as lacking in some required area by the other. As a result, the supervisee feels somewhat wary or careful about their words and actions around the supervisor, feeling that they could be corrected in an area they feel insecure about.',
-    'Partners like to discuss similar topics and often have many common interests. But when they come to practical implementation of their mutual goals and projects, each finds that the other acts in contrary ways.',
-    'Relationships of semi-duality can become very close for moderate periods of time. Often these relations lead to friendship and fruitful cooperation at work. But very close relationships can lead to disappointments.'
-]
+def get_relation_name(code1, code2, lang='en'):
+    """Calculate the name of the relationship between two socionic types."""
+    relation_index = get_relation_index(code1, code2)
+    return RELATIONSHIP_NAMES[lang][relation_index]
 
-TYPE_ABBREVIATIONS = {
-    'ILE': 0, 'SEI': 1, 'ESE': 2, 'LII': 3, 'EIE': 4, 'LSI': 5, 'SLE': 6, 'IEI': 7,
-    'SEE': 8, 'ILI': 9, 'LIE': 10, 'ESI': 11, 'LSE': 12, 'EII': 13, 'IEE': 14, 'SLI': 15
-}
+def get_relation_description(code1, code2, lang='en'):
+    """Get the description of the relationship between two socionic types."""
+    relation_index = get_relation_index(code1, code2)
+    return RELATIONSHIP_DESCRIPTIONS[lang][relation_index]
 
-TYPE_NAMES = {
-    0: 'Innovator', 1: 'Mediator', 2: 'Enthusiast', 3: 'Analyst', 4: 'Mentor', 5: 'Inspector',
-    6: 'Marshal', 7: 'Romantic', 8: 'Leader', 9: 'Critic', 10: 'Entrepreneur', 11: 'Moralist',
-    12: 'Pragmatist', 13: 'Humanist', 14: 'Networker', 15: 'Ergonomist'
-}
-
-def calculate_type_relationship(type1, type2):
+def calculate_type_relationship(first_type, second_type, lang='en'):
     """Calculate and display the relationship between two socionic types."""
-    code1, code2 = TYPE_ABBREVIATIONS[type1.upper()], TYPE_ABBREVIATIONS[type2.upper()]
-    relation_index = abs(code1 - code2) % 16
-    relation_name = TYPE_RELATIONSHIPS[relation_index]
-    print(f"\nIntertype relationship between {type1.upper()} ({TYPE_NAMES[code1]}) and "
-          f"{type2.upper()} ({TYPE_NAMES[code2]}) - {relation_name} Relationship")
-    print("Description:", TYPE_DESCRIPTIONS[relation_index])
+    code1, code2 = TYPE_ABBREVIATIONS[first_type.upper()], TYPE_ABBREVIATIONS[second_type.upper()]
+    relation_name = get_relation_name(code1, code2, lang)
+    relation_description = get_relation_description(code1, code2, lang)
+    type_name1, type_name2 = TYPE_NAMES[code1][lang], TYPE_NAMES[code2][lang]
+    print(f"\nIntertype relationship between {first_type.upper()} ({type_name1}) and "
+          f"{second_type.upper()} ({type_name2}) - {relation_name} Relationship")
+    print("Description:", relation_description)
 
 if __name__ == "__main__":
-    print("Welcome to the Socionics Intertype Relationship Calculator!")
+    lang = input("Choose a language (en/es/uk): ").lower()
+    if lang not in RELATIONSHIP_NAMES:
+        print("Invalid language choice. Defaulting to English (en).")
+        lang = 'en'
+
+    print(f"Welcome to the Socionics Intertype Relationship Calculator! (Language: {lang})")
     while True:
-        type1 = input("\nEnter the first type (e.g., 'SEI' or 'Mediator'): ").upper()
-        if type1 in TYPE_ABBREVIATIONS:
-            type2 = input("Enter the second type (e.g., 'SEI' or 'Mediator'): ").upper()
-            if type2 in TYPE_ABBREVIATIONS:
-                calculate_type_relationship(type1, type2)
+        first_type = input("\nEnter the first type (e.g., 'SEI' or 'Mediator'): ").upper()
+        if first_type in TYPE_ABBREVIATIONS:
+            second_type = input("Enter the second type (e.g., 'SEI' or 'Mediator'): ").upper()
+            if second_type in TYPE_ABBREVIATIONS:
+                calculate_type_relationship(first_type, second_type, lang)
             else:
                 print("Invalid type abbreviation. Please enter a valid type abbreviation.")
         else:
---
+++ socionics_types.py
@@ -0,0 +1,23 @@
+TYPE_ABBREVIATIONS = {
+    'ILE': 0, 'SEI': 1, 'ESE': 2, 'LII': 3, 'EIE': 4, 'LSI': 5, 'SLE': 6, 'IEI': 7,
+    'SEE': 8, 'ILI': 9, 'LIE': 10, 'ESI': 11, 'LSE': 12, 'EII': 13, 'IEE': 14, 'SLI': 15
+}
+
+TYPE_NAMES = {
+    0: {'en': 'Innovator', 'es': 'Innovador', 'uk': 'Інноватор'},
+    1: {'en': 'Mediator', 'es': 'Mediador', 'uk': 'Медіатор'},
+    2: {'en': 'Enthusiast', 'es': 'Entusiasta', 'uk': 'Ентузіаст'},
+    3: {'en': 'Analyst', 'es': 'Analista', 'uk': 'Аналітик'},
+    4: {'en': 'Mentor', 'es': 'Mentor', 'uk': 'Наставник'},
+    5: {'en': 'Inspector', 'es': 'Inspector', 'uk': 'Інспектор'},
+    6: {'en': 'Marshal', 'es': 'Mariscal', 'uk': 'Маршал'},
+    7: {'en': 'Romantic', 'es': 'Romántico', 'uk': 'Романтик'},
+    8: {'en': 'Leader', 'es': 'Líder', 'uk': 'Лідер'},
+    9: {'en': 'Critic', 'es': 'Crítico', 'uk': 'Критик'},
+    10: {'en': 'Entrepreneur', 'es': 'Emprendedor', 'uk': 'Підприємець'},
+    11: {'en': 'Moralist', 'es': 'Moralista', 'uk': 'Мораліст'},
+    12: {'en': 'Pragmatist', 'es': 'Pragmático', 'uk': 'Прагматик'},
+    13: {'en': 'Humanist', 'es': 'Humanista', 'uk': 'Гуманіст'},
+    14: {'en': 'Networker', 'es': 'Comunicador', 'uk': 'Комунікатор'},
+    15: {'en': 'Ergonomist', 'es': 'Ergónomo', 'uk': 'Ергоном'}
+}
