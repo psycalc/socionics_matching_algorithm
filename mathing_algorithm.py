76daff0874be80303bec5d58c9543214ad506920
Oleh Protsenko <designofhumanity@gmail.com>
2023-07-29 11:14:34 +0300

    this file no needed

--- mathing_algorithm.py
+++
@@ -1,8 +0,0 @@
-from socionics import SocionicsType
-
-type1 = SocionicsType('INTj')
-type2 = SocionicsType('ESFp')
-
-relation = type1.relation_to(type2)
-
-print(f"The intertype relation between {type1} and {type2} is {relation}.")
